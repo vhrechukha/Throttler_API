9cfd78260ef0279548721316b934fd12163756dd
melkisa <vik.coder@gmail.com>
2021-08-20 21:03:04 +0300

    refactor: fixed export

--- src/crons.ts
+++ src/crons.ts
@@ -3,6 +3,4 @@ import { CronJob } from 'cron';
 import { state } from './index';
 import service from './service';
 
-const clearOldDataEvery5Minute = new CronJob('*/5 * * * *', () => service.clearOldData(state, Date.now()));
-
-export { clearOldDataEvery5Minute };
+export const clearOldDataEvery5Minute = new CronJob('*/5 * * * *', () => service.clearOldData(state, Date.now()));
